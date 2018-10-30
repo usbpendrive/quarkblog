d640a15e57b389d33cd600c6a44fc86ac1a909bf
Fernando <ferry.fernando@gmail.com>
2018-10-30 10:26:48 +0700

    Tag system.

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 
-from blog.models import Post
+from blog.models import Post, Tag
 
 
 def home(request):
@@ -10,3 +10,10 @@ def home(request):
     page = request.GET.get('page')
     items = paginator.get_page(page)
     return render(request, 'base/home.html', {'items': items, 'title': 'Home'})
+
+
+def tag(request, slug=None):
+    _tag = get_object_or_404(Tag, slug=slug)
+    items = Post.objects.filter(tags__slug=slug)
+    title = 'Items tagged with "%s"' % _tag
+    return render(request, 'base/tag.html', {'items': items, 'tag': tag, 'title': title})
