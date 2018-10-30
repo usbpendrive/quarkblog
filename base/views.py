ec8d5442d0e707288f7d5f49a1cfbd08b114b4f6
Fernando <ferry.fernando@gmail.com>
2018-10-30 10:05:26 +0700

    Implement page pagination.

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from blog.models import Post
 
 
 def home(request):
     items = Post.objects.all()
+    paginator = Paginator(items, 3)
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
     return render(request, 'base/home.html', {'items': items, 'title': 'Home'})
