076204e3f7969293709f658bfc61980e5a5bad72
Fernando <ferry.fernando@gmail.com>
2018-10-31 07:38:21 +0700

    User profile edit function.

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField, UsernameField
 from django.contrib.auth.models import User
 
 from base.models import Profile
@@ -29,3 +29,40 @@ class CustomUserCreationForm(UserCreationForm):
             profile.save()
 
             return user
+
+
+class CustomUserChangeForm(UserChangeForm):
+    email = forms.EmailField(label=_("Email"), help_text=_("Enter a valid email address"), )
+    displayname = forms.CharField(label=_("Displayname"), help_text=_("Enter displayname"), required=False, )
+
+    password = ReadOnlyPasswordHashField(label=_("Password"),
+                                         help_text=_("Raw passwords are not stored, so there's no way to see this"
+                                                     "user's password, but you can change the password using "
+                                                     "<a href=\"/accounts/password_change/\">this form</a>"))
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "displayname", "password")
+        field_classes = {'username': UsernameField}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.profile = Profile.objects.get(user=self.instance)
+        self.fields['displayname'].initial = self.profile.display_name
+        f = self.fields.get('user_pemissions')
+
+        if f is not None:
+            f.queryset.select_related('content-type')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+            self.profile.display_name = self.cleaned_data["displayname"]
+            self.profile.save()
+            return user
+
+    def clean_password(self):
+        return self.initial["password"]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from blog.models import Post, Tag
-from base.forms import CustomUserCreationForm
+from base.forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 def home(request):
@@ -32,3 +34,13 @@ class Register(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserChange(LoginRequiredMixin, generic.UpdateView):
+    model = User
+    form_class = CustomUserChangeForm
+    template_name = 'registration/user_change.html'
+    success_url = reverse_lazy('login')
+
+    def get_object(self):
+        return self.request.user
